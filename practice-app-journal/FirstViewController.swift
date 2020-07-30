0f3813b5edd89b5001cb42d0810501215f4758f7
Mikaela Foronda <mikaela.foronda@icloud.com>
2020-07-29 19:36:24 -0700

    Initial Commit

---
+++ practice-app-journal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  practice-app-journal
+//
+//  Created by Mikaela Foronda Zanipatin on 7/29/20.
+//  Copyright © 2020 Mikaela Foronda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
